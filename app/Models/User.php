03a65c31a2ac6e9c9417a0d7096979dd93e8f474
vagrant <vagrant@homestead>
2020-05-22 01:53:08 +0000

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function addresses()
+    {
+        //默认第二个参数的外键名为 User模型小写 加上 _id 即 user_id 作为外键
+        return $this->hasMany(UserAddress::class);
+    }
+
+
 }
