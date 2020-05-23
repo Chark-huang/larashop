153f55c76b232be61e1fb09e609452949860cb3d
vagrant <vagrant@homestead>
2020-05-23 07:42:52 +0000

    商品模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
     use DefaultDatetimeFormat;
-    
+
     /**
      * The attributes that are mass assignable.
      *
