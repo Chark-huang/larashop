52351af5ab50b1393cf791a125aa8eca6b3e460f
vagrant <vagrant@homestead>
2020-05-24 14:39:25 +0000

    关闭未支付订单

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Exceptions\InternalException;
 use App\Exceptions\InvalidRequestException;
 use App\Http\Requests\OrderRequest;
+use App\Jobs\CloseOrder;
 use App\Models\Order;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
@@ -64,7 +65,8 @@ class OrdersController extends Controller
 
             return $order;
         });
-
+        //触发定时关闭订单任务
+        $this->dispatch(new CloseOrder($order,config('app.order_ttl')));
         return $order;
     }
 }
