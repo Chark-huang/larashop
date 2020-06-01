27264aba26e13c41fdd53d9269ea7511864c2625
vagrant <vagrant@homestead>
2020-06-01 08:59:53 +0000

    前台分期付款详情

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
 
     //分期付款
     Route::get('installments','InstallmentsController@index')->name('installments.index');
+    Route::get('installments/{installment}','InstallmentsController@show')->name('installments.show');
     Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 
